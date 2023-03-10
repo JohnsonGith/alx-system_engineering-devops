e3fd21f4600a1706f1bc453bfce5341a62edf44a
JohnsonGith <johnsonwebsax@gmail.com>
2023-03-10 00:20:09 +0000

    file updated

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # Executes a bash command
 exec { 'kill':
   command => 'pkill -f killmenow',
-  path    => ['/usr/bin', '/usr/sbin']
+  path    => ['/usr/bin', '/usr/sbin'],
+  onlyif  => 'pgrep -f killmenow',
 }
